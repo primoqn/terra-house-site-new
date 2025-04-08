d84715a30520dfb7815e53a3f5251db8a482a7e9
primoqn <turculet.mihai@gmail.com>
2025-04-08 16:02:15 +0300

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 text-center text-sm text-gray-500 p-4">
-      &copy; {new Date().getFullYear()} Terra House Apartments · trhsuites.ro
-    </footer>
-  );
-}

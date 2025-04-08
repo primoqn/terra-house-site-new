44260fe6cd24403faeed45581077779b70bfd7d0
primoqn <turculet.mihai@gmail.com>
2025-04-08 15:36:00 +0300

    Add files via upload

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,8 +6,10 @@ export default function Contact() {
       <div className="w-full h-64">
         <iframe
           className="w-full h-full rounded-xl"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2848.333... (scurtat)"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2848.3127790233645!2d26.12365407655176!3d44.44110667107585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b1ff4ce960b351%3A0x9c1b3dd6c11b249b!2sTerra%20House%20Apartments!5e0!3m2!1sen!2sro!4v1712560000000!5m2!1sen!2sro"
           loading="lazy"
+          allowFullScreen=""
+          referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
       <div className="text-center">
